02f2c8bf43a4e6d68c42e80597c4a0ef026a0c0d
Radovan Ostojic <Radovan.Ostojic@Vanderlande.com>
2018-10-17 15:11:31 +0200

    draft async OK
    draft async OK

--- TelegramGrouping/Models/Telegrams.cs
+++ TelegramGrouping/Models/Telegrams.cs
@@ -14,38 +14,5 @@ namespace DataGridGroupDemo.Models
 		public string TelegramId	{set; get;}
 		public string FileLine		{set; get;}
 		public string FileName		{set; get;}
-	
-			
-		public string noGroup {
-			get {
-				return "Total";
-			}
-		}
 	}
-	
-	public class Telegrams: ObservableCollection<Telegram>
-	{
-		public Telegrams(string[] fileNames)
-		{
-            foreach (var file in fileNames)
-            {
-                string[] allLInes = File.ReadAllLines(file);
-                foreach (var line in allLInes)
-                {
-                    foreach (var pattern in TelegramConstants.AllPatterns)
-                    {
-                        if (Regex.IsMatch(line, pattern))
-                        {
-                            this.Add(new Telegram
-                            {
-                                TelegramId = pattern.Substring(pattern.Length - 2),
-                                FileLine = line,
-                                FileName = file
-                            });
-                        }
-                    }
-                }
-            }
-        }
-    }
 }
--- TelegramGrouping/Window1.xaml.cs
+++ TelegramGrouping/Window1.xaml.cs
@@ -6,9 +6,18 @@
  * 
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
+using DataGridGroupDemo.Models;
+using DataGridGroupDemo.ViewModels;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -22,11 +31,127 @@ namespace DataGridGroupDemo
 	/// Interaction logic for Window1.xaml
 	/// </summary>
 	public partial class Window1 : Window
-	{
-		public Window1()
+    {
+        ObservableCollection<Telegram> Telegrams;
+
+        CancellationTokenSource cancelToken;
+
+        Progress<double> progressOperation;
+    
+
+        public Window1()
 		{
 			InitializeComponent();
-			DataContext = new ViewModels.TelegramsViewModel();
-		}
+            Telegrams = new ObservableCollection<Telegram>();
+        }
+
+
+        async Task<ObservableCollection<Telegram>> LoadTelegramsAsync(CancellationToken ct, IProgress<double> progress)
+        {
+            Telegrams.Clear();
+            
+            var task = Task.Run(async () => {
+                int recCount = 0;
+                foreach (var file in TelegramsViewModel.fileNames)
+                {
+                    string[] allLInes = await GetAllLines(file);
+                    long total = allLInes.GetLongLength(0);
+                    foreach (var line in allLInes)
+                    {
+                        foreach (var pattern in TelegramConstants.AllPatterns)
+                        {
+                            if (Regex.IsMatch(line, pattern))
+                            {
+                                Telegrams.Add(new Telegram
+                                {
+                                    TelegramId = pattern.Substring(pattern.Length - 2),
+                                    FileLine = line,
+                                    FileName = file
+                                });
+                            }
+                        }
+                        ++recCount;
+                        progress.Report(recCount * 100.0 / total);
+                    }
+                    recCount = 0;
+                    progress.Report(recCount);
+                }
+
+                return Telegrams;
+
+            });
+
+            return await task;
+        }
+
+        async Task<string[]> GetAllLines(string fileName)
+        {
+            using (var reader = File.OpenText(fileName))
+            {
+                string fileText = await reader.ReadToEndAsync();
+                string[] allLInes = fileText.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+                return allLInes;
+            }
+        }
+
+        private bool  DoLoadFiles()
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                Multiselect = true,
+                Filter = "Log files (*.log)|*.log|All files (*.*)|*.*",
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                foreach (string filename in openFileDialog.FileNames)
+                {
+                    TelegramsViewModel.fileNames.Add(filename);
+                }
+                return  true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            bool go =  DoLoadFiles();
+            if (!go)
+            {
+                return;
+            }
+            
+            cancelToken = new CancellationTokenSource();
+            btnLoadFiles.IsEnabled = false;
+            progressOperation = new Progress<double>(value => progress.Value = value);
+            try
+            {
+                var Telgs = await LoadTelegramsAsync(cancelToken.Token, progressOperation);
+
+                foreach (var item in Telgs)
+                {
+                    dgTel.Items.Add(item);
+                }
+            }
+            catch (OperationCanceledException ex1)
+            {
+
+                throw;
+            }
+            catch (Exception ex2)
+            {
+                throw;
+                //txtstatus.Text = "Operation cancelled" + ex.Message;
+            }
+            finally
+            {
+                cancelToken.Dispose();
+                btnLoadFiles.IsEnabled = true;
+                //btnCancel.IsEnabled = false;
+            }
+        }
 	}
 }
